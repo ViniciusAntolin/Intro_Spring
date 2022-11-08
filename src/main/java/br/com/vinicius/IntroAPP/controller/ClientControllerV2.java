c0607f2ac9878543358dc010da53be7d4b85d6d3
0 <0>
2022-11-08 10:38:17 -0300

    Adicionada lógica para passar/aceitar xml

--- src/main/java/br/com/vinicius/IntroAPP/controller/ClientControllerV2.java
+++ src/main/java/br/com/vinicius/IntroAPP/controller/ClientControllerV2.java
@@ -3,7 +3,11 @@ package br.com.vinicius.IntroAPP.controller;
 import br.com.vinicius.IntroAPP.model.ClientModel;
 import br.com.vinicius.IntroAPP.service.ClientServiceV1;
 import br.com.vinicius.IntroAPP.service.ClientServiceV2;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,22 +21,31 @@ public class ClientControllerV2 {
     @Autowired
     private ClientServiceV2 service;
 
-    @GetMapping("/{id}")
-    public Optional<ClientModel> findById(@PathVariable("id") long id) {
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    @ApiOperation(value = "Returns a Client by IO")
+    public Optional<ClientModel> findById(
+            @ApiParam(name = "id", value = "A valid integer value", required = true)
+            @PathVariable("id") long id) {
         return service.findById(id);
     }
 
-    @GetMapping
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
     public List<ClientModel> findAll() {
         return service.findAll();
     }
 
-    @PostMapping
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ClientModel save(@RequestBody ClientModel model) {
         return service.save(model);
     }
 
-    @PutMapping
+    @PutMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ClientModel update(@RequestBody ClientModel model){
         return service.update(model);
     }
